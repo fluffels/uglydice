26f9a8dc9930be972059e7dd2532e58830d57408
Jan CW Kroezee <thejcwk@gmail.com>
2018-01-16 15:04:36 +0200

    Modifer is optional.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -24,7 +24,7 @@ export class HomePage {
             let modifier = parseInt(parts[1]);
             console.log(`${count} ${sides} sided dice plus ${modifier}`);
             let lastRoll = '';
-            let total = modifier;
+            let total = 0;
             for (let die = 0; die < count; die++) {
                 let roll = 1 + Math.round((Math.random() * sides));
                 if (die == 0) {
@@ -34,7 +34,11 @@ export class HomePage {
                 }
                 total += roll;
             }
-            lastRoll += " + " + modifier + " = " + total;
+            if (isNaN(modifier) === false) {
+                total += modifier;
+                lastRoll += " + " + modifier;
+            }
+            lastRoll += " = " + total;
             document.getElementById("last-roll").innerText = lastRoll;
             console.log(total);
         } else {
