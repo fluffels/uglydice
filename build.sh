cca4b20c2c022d4bd9a0bb8272d985adfde842e0
Jan CW Kroezee <thejcwk@gmail.com>
2018-01-19 13:23:02 +0200

    Icon / splash.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+ionic cordova resources android
 ionic cordova build android --prod --release && \
 cp platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk unsigned.apk && \
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.jks unsigned.apk my-alias && \
