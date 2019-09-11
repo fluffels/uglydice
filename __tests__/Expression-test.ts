70c10db0ebbbe2201d9afb3c0e8611221f2fdea2
Jan CW Kroeze <jcwkroeze@pm.me>
2019-09-11 12:12:38 +0200

    Test random expressions with addition

--- __tests__/Expression-test.ts
+++ __tests__/Expression-test.ts
@@ -19,3 +19,16 @@ it('calculates simple random expressions', () => {
   expect(result).toBeLessThanOrEqual(20);
   expect(result).toBeGreaterThanOrEqual(1);
 });
+
+it('calculates random expressions with addition', () => {
+  let e = new Expression();
+  e.consumeToken(1);
+  e.consumeToken('d');
+  e.consumeToken(2);
+  e.consumeToken(0);
+  e.consumeToken('+')
+  e.consumeToken(5)
+  let result = e.compute();
+  expect(result).toBeLessThanOrEqual(25);
+  expect(result).toBeGreaterThanOrEqual(6);
+});
